d734e46024b8e54f38b78ae28ade292c3bd65ae5
jacklion710 <jack.lion710@gmail.com>
2024-06-21 00:01:20 -0400

    Ping pong delay

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -182,8 +182,10 @@ void DelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, [[mayb
         float dryL = channelDataL[sample];
         float dryR = channelDataR[sample];
         
-        delayLine.pushSample(0, dryL + feedbackL);
-        delayLine.pushSample(1, dryR + feedbackR);
+        float mono = (dryL + dryR) * 0.5; // Convet stereo to mono
+        
+        delayLine.pushSample(0, mono*params.panL + feedbackR);
+        delayLine.pushSample(1, mono*params.panL + feedbackL);
         
         float wetL = delayLine.popSample(0);
         float wetR = delayLine.popSample(1);
